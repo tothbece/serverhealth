91b7c0e32afab9025b1f96efaa4aa96c79a26754
tothbece <tothbece@lauder.hu>
2023-04-01 17:05:25 +0200

    enable CORS from localhost:4200

--- server/serverHealthService.js
+++ server/serverHealthService.js
@@ -1,6 +1,13 @@
 const express = require('express'); // for web services
 const os = require("os");
 
+// enable Cross Origin Resource Sharing
+const cors = require("cors");
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200
+}
+
 /**
  * Simple webservice providing information about the server.
  */
@@ -8,6 +15,7 @@ class ServerHealthService {
 
   constructor(app) {
     this.app = app || express(); // receiving mocked object or creating new express application
+    this.app.use(cors(corsOptions));
     this.app.get('/status', this.getStatus.bind(this));
   }
 
