e23715d14595be650dcc3e53bdd95f6213a13c78
tothbece <tothbece@lauder.hu>
2023-04-01 18:21:28 +0200

    real time functionality

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HealthData} from "./models/HealthData";
 import {ServerHealthService} from "./services/server-health.service";
+import {interval, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy { // OnInit & OnDestroy needed for the interval implementation
+  private readonly onDestroySubject = new Subject<void>();
+
   title = 'webapp';
   public data: HealthData | null; // last data from server
   public log: HealthData[] = [] // all previous data
   constructor(private service: ServerHealthService) {
     this.data = null;
+  }
+
+  ngOnInit() { // starting the interval
+    interval(5000)
+      .pipe(
+        takeUntil(this.onDestroySubject)
+      )
+      .subscribe(() => {
+        this.loadData();
+      });
+  }
+
+  ngOnDestroy() { // stopping the interval
+    this.onDestroySubject.next();
+    this.onDestroySubject.complete();
   }
 
   /**
